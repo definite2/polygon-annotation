682733288b64e40c88d7b5a40939e83dbed7a99c
definite2 <edefne878@gmail.com>
2023-09-10 13:20:25 +0300

    refactor: update example

--- src/example/Example.tsx
+++ src/example/Example.tsx
@@ -1,15 +1,31 @@
 import { useState } from 'react';
-import { PolygonAnnotation } from 'lib';
+import { PolygonAnnotation, PolygonConfigProps } from 'lib';
+
 import Toolbar from './Toolbar';
 
 const videoSource = './space_landscape.jpg';
 
 const AnnotationDraw = () => {
   const [maxPolygons, setMaxPolygons] = useState<number>(1);
+  const [config, setConfig] = useState<PolygonConfigProps>({
+    vertexRadius: 6,
+    lineColor: '#1ea703',
+    fillColor: '#37f71139',
+    vertexColor: '#ff0000',
+  });
   return (
     <>
-      <PolygonAnnotation bgImage={videoSource} maxPolygons={maxPolygons}>
-        <Toolbar maxPolygons={maxPolygons} setMaxPolygons={setMaxPolygons} />
+      <PolygonAnnotation
+        bgImage={videoSource}
+        maxPolygons={maxPolygons}
+        config={config}
+      >
+        <Toolbar
+          maxPolygons={maxPolygons}
+          setMaxPolygons={setMaxPolygons}
+          config={config}
+          setConfig={setConfig}
+        />
       </PolygonAnnotation>
     </>
   );
--- src/example/Toolbar.tsx
+++ src/example/Toolbar.tsx
@@ -1,13 +1,19 @@
 import { useUndoRedo } from 'lib/useDrawHistory';
 import { useGetPolygonData } from 'lib/useGetPolygonData';
 import './Toolbar.css';
+import { PolygonConfigProps } from 'lib';
 
 const Toolbar = ({
   maxPolygons,
   setMaxPolygons,
+  config,
+  setConfig,
 }: {
   maxPolygons: number;
+  config: PolygonConfigProps;
+
   setMaxPolygons: (maxPolygons: number) => void;
+  setConfig: (config: PolygonConfigProps) => void;
 }) => {
   const { undo, redo, canUndo, canRedo } = useUndoRedo();
   const { polygons } = useGetPolygonData();
@@ -21,6 +27,50 @@ const Toolbar = ({
           placeholder="Enter max number of polygons"
           value={maxPolygons}
           onChange={(e) => setMaxPolygons(+e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="lineColor">Polygon Line Color: </label>
+        <input
+          id="lineColor"
+          type="color"
+          placeholder="Enter polygon line color"
+          value={config.lineColor}
+          onChange={(e) => setConfig({ ...config, lineColor: e.target.value })}
+        />
+      </div>
+      <div>
+        <label htmlFor="vertexColor">Vertex Fill Color: </label>
+        <input
+          id="vertexColor"
+          type="color"
+          placeholder="Enter polygon vertex color"
+          value={config.vertexColor}
+          onChange={(e) =>
+            setConfig({ ...config, vertexColor: e.target.value })
+          }
+        />
+      </div>
+      <div>
+        <label htmlFor="vertexColor">Polygon Fill Color: </label>
+        <input
+          id="fillColor"
+          type="text"
+          placeholder="Enter polygon fill color"
+          value={config.fillColor}
+          onChange={(e) => setConfig({ ...config, fillColor: e.target.value })}
+        />
+      </div>
+      <div>
+        <label htmlFor="vertexRad">Vertex Radius: </label>
+        <input
+          id="vertexRad"
+          type="number"
+          placeholder="Enter polygon fill color"
+          value={config.vertexRadius}
+          onChange={(e) =>
+            setConfig({ ...config, vertexRadius: +e.target.value })
+          }
         />
       </div>
       <div>
